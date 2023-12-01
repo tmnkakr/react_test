f3d507fe3415a088ada50d5292ecd7d164e6cde3
tmnkakr <tmnkakr@gmail.com>
2023-12-02 02:14:54 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,28 +3,22 @@ import './App.css';
 
 function App() {
   async function a() {
-    const response = await fetch('http://16.171.238.101:81/user/listtemplate/1');
-    alert(response.status);
+    try {
+      const response = await fetch('http://16.171.238.101:81/user/listtemplate/1');
+      alert(response.status);
+    } catch (e) {
+      console.log("e " + e);
+    }
 
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={() => { a(); }} >click</button>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div >
   );
